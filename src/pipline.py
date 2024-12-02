14ad7839d2a1707f6689d9ae2db09ad7d939a491
dlrjsgh367 <dlrjsgh367@gmail.com>
2024-12-02 17:43:39 +0900

    Feat: 매핑 코드

--- src/pipline.py
+++ src/pipline.py
@@ -1,6 +1,7 @@
 import os
 
 from task_mapping import map_to_string
+from task_gspread import sheet
 
 
 def pl(dir_path, mode=None):
@@ -10,14 +11,18 @@ def pl(dir_path, mode=None):
                 file_path = os.path.join(folder, file_name)
 
     elif mode == "m":
+        sheet_records = sheet.get_all_records()
+
         for folder, _, file_names in os.walk(dir_path):
             for file_name in file_names:
                 file_path = os.path.join(folder, file_name)
-                map_to_string()
+                map_to_string(file=file_path, sheet_records=sheet_records)
+                # print(file_path, sheet_records)
+
     else:
         quit()
 
 
 if __name__ == "__main__":
-    dir_path = r"C:\Users\HAMA\workspace\lang-apply-py\features"
+    dir_path = r"C:\Users\HAMA\workspace\lang-apply-py\features\worker"
     pl(dir_path, "m")
--- src/task_mapping.py
+++ src/task_mapping.py
@@ -1,3 +1,35 @@
-def map_to_string(file):
-    
-    with open()
+import re
+
+
+def map_to_string(file, sheet_records):
+
+    with open(file, "r", encoding="utf-8") as doc:
+        lines = doc.readlines()
+
+    results = []
+    for line in lines:
+        result = process_line(line, file, sheet_records)
+        results.append(result)
+        # print(line)
+
+    # with open(file, "w", encoding="utf-8") as output:
+    #     output.writelines()
+
+
+def process_line(line, output, sheet_records, basename="localization.{}"):
+    pattern_x = re.compile(r"['\"]([^'\"]*?[가-힣]+[^'\"]*?)['\"]")
+
+    matches = pattern_x.findall(line)
+    if not matches:
+        return line
+    for match in matches:
+        for sheet_record in sheet_records:
+            if match == sheet_record.get("ko"):
+                key = sheet_record.get("key")
+                replacement = basename.format(key)
+                print(line)
+                if "'" in line:
+                    line = line.replace(f"'{match}'", replacement)
+                elif '"' in line:
+                    line = line.replace(f'"{match}"', replacement)
+                print(line)
